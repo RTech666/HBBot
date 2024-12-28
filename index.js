bd96d9dd0fd5f33c7e596d98dbd02696ed615e37
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-12-28 13:09:36 -0500

    Added an event handler.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const {Client} = require("discord.js");
-const client = new Client({intents: ["Guilds"]});
+const {Client, GatewayIntentBits, Partials, Collection} = require("discord.js");
+const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
+const {User, Message, GuildMember, ThreadMember} = Partials;
+
+const client = new Client({
+    intents: [Guilds, GuildMembers, GuildMessages],
+    partials: [User, Message, GuildMember, ThreadMember],
+});
+
+const {loadEvents} = require("./Handlers/eventHandler");
+loadEvents(client);
 
 client.config = require("./config.json");
+client.events = new Collection();
 
 client.login(client.config.token).then(() => {
     console.log(`${client.user.username} logged in successfully!`);
