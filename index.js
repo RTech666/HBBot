112be434495db2c6ed61b8246e5a8aad8cc0632c
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-12-28 12:13:00 -0500

    Fixed typos.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const {Client} = require("discord.js");
-const client = new client({intents: ["Guilds"]});
+const client = new Client({intents: ["Guilds"]});
 
 client.config = require("./config.json");
 
 client.login(client.config.token).then(() => {
     console.log(`${client.user.username} logged in successfully!`);
-    client.user.setActivity(`with ${client.guilds.cahce.size} guilds`);
+    client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
 }).catch((err) => console.log(err));
